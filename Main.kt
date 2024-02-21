528f8175a6f5a844a48076c2c503a7f1874637e3
Annetjepkorirr <you@example.com>
2024-02-21 08:14:17 +0300

    add file

---
+++ Main.kt
@@ -0,0 +1,30 @@
+  fun main() {
+      greetingsname ("Mercy")
+      var x = modulus(40,15.0)
+      var y = add(20,30,40,50)
+      intrestingythingaboutme("I love outdoor activities")
+
+
+
+
+  }
+  fun greetingsname(name: String){
+      var name = "Hello ${name}"
+      println(name)
+  }
+
+      fun modulus(num1:Int ,num2:Double):Double{
+          println(num1%num2)
+          return num1 % num2
+      }
+  fun add(num1:Int,num2:Int,num3:Int,num4:Int):Int{
+      println(num1+num2+num3+num4)
+      return num1+num2+num3+num4
+  }
+  fun intrestingythingaboutme(Fact:String){
+      var Fact= "I love outdoor activities"
+      println(Fact)
+  }
+
+
+
