aadfb93e86f40562e969054a896d088ed950e970
Mercy-Chemtai <chemtailodite@gmail.com>
2024-02-26 22:08:16 +0300

    commited changes

--- Main.kt
+++ Main.kt
@@ -1,30 +1,32 @@
   fun main() {
-      greetingsname ("Mercy")
+      greetingsname ("Hello Mercy")
       var x = modulus(40,15.0)
       var y = add(20,30,40,50)
-      intrestingythingaboutme("I love outdoor activities")
+      fact ("I love outdoor activities")
 
 
 
 
   }
   fun greetingsname(name: String){
-      var name = "Hello ${name}"
       println(name)
   }
 
       fun modulus(num1:Int ,num2:Double):Double{
-          println(num1%num2)
+          var reminder = num1%num2
+          println(reminder)
           return num1 % num2
       }
   fun add(num1:Int,num2:Int,num3:Int,num4:Int):Int{
-      println(num1+num2+num3+num4)
-      return num1+num2+num3+num4
+      var result =num1+num2+num3+num4
+      println(result)
+      return result
   }
-  fun intrestingythingaboutme(Fact:String){
-      var Fact= "I love outdoor activities"
-      println(Fact)
+  fun fact(intrestingthingaboutme:String){
+      var fact= "I love outdoor activities"
+      println(fact)
+
   }
-//hello I am trying to fix my github
+
 
 
